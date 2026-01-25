740b48428d7391203021b134904c2f4861d06c22
Swatistar <swatimva6@gmail.com>
2026-01-25 18:55:15 -0500

    Add files via upload

---
+++ Shapehw.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.Screen().bgcolor("Green")
+board = turtle.Turtle()
+# Triangle
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+# Retrangle
+board.forward(100)
+board.left(90)
+board.forward(160)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(160)
+board.left(90)
